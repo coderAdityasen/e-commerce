8fd4e9c386b4d478f5836938360053071298b98a
Azeemuddin Aziz <azeemuddinaziz@Azeemuddins-MacBook-Air.local>
2024-03-03 22:39:00 +0530

    fix add to wishlist button
    but add to cart functionality on wishlist page is not functional

--- src/Components/WishList.jsx
+++ src/Components/WishList.jsx
@@ -1,13 +1,22 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { CartContext } from '../Context/productContext';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../Context/productContext";
 
 function WishList() {
-  const { cartItems, wishlistItems, addToCart, removeFromCart, removeFromWishlist, clearWishlist } = useContext(CartContext);
+  const {
+    cartItems,
+    wishlistItems,
+    addToCart,
+    removeFromCart,
+    removeFromWishlist,
+    clearWishlist,
+  } = useContext(CartContext);
 
   const handleRemoveFromCart = (product) => {
     removeFromCart(product);
   };
+
+  console.log(cartItems);
 
   return (
     <div>
@@ -30,15 +39,20 @@ function WishList() {
 
       {/* Wishlist items */}
       <div>
-        <button onClick={() => clearWishlist()}>
-          Clear list
-        </button>
+        <button onClick={() => clearWishlist()}>Clear list</button>
       </div>
-      <div className='my-24 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10'>
-        {wishlistItems.map((product) => {
-          return   product.wishlist ? (
-            <div key={product.id} className="bg-white shadow-md rounded-lg px-10 py-10">
-              <img src={product.thumbnail} alt={product.title} className="rounded-md h-48" />
+      <div className="my-24 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10">
+        {wishlistItems.map((product, index) => {
+          return product.wishlist ? (
+            <div
+              key={index}
+              className="bg-white shadow-md rounded-lg px-10 py-10"
+            >
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+                className="rounded-md h-48"
+              />
               <div className="mt-4">
                 <h1 className="text-lg uppercase font-bold">{product.title}</h1>
                 <p className="mt-2 text-gray-600 text-sm">
@@ -46,42 +60,61 @@ function WishList() {
                 </p>
                 <p className="mt-2 text-gray-600">${product.price}</p>
               </div>
-              <div className="mt-6 flex justify-between items-center">
-                {!cartItems.find((item) => item.id === product.id) ? (
-                  <button className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+              <button
+                className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+                onClick={() => {
+                  addToCart(product);
+                  notifyAddedToCart(product);
+                }}
+              >
+                Add to cart
+              </button>
+
+              {/* <div className="mt-6 flex justify-between items-center">
+                {cartItems !== undefined &&
+                cartItems.find((item) => item.id === product.id) ? (
+                  <button
+                    className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
                     onClick={() => {
                       addToCart(product);
                       notifyAddedToCart(product);
-                    }}>
+                    }}
+                  >
                     Add to cart
                   </button>
                 ) : (
                   <div className="flex gap-4">
-                    <button className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-                      onClick={() => addToCart(product)}>
+                    <button
+                      className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+                      onClick={() => addToCart(product)}
+                    >
                       +
                     </button>
                     <p className="text-gray-600">
-                      {cartItems.find((item) => item.id === product.id)?.quantity || 0}
+                      {cartItems.find((item) => item.id === product.id)
+                        ?.quantity || 0}
                     </p>
-                    <button className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+                    <button
+                      className="px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
                       onClick={() => {
                         handleRemoveFromCart(product);
                         removeFromCart(product);
-                      }}>
+                      }}
+                    >
                       -
                     </button>
                   </div>
                 )}
-                <button onClick={() => removeFromWishlist(product)} className="px-4 py-2 mx-3">
+                <button
+                  onClick={() => removeFromWishlist(product)}
+                  className="px-4 py-2 mx-3"
+                >
                   Remove From Wishlist
                 </button>
-              </div>
+              </div> */}
             </div>
-          ) : null
-       }
-        )
-        }
+          ) : null;
+        })}
       </div>
     </div>
   );
